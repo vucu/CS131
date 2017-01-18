7c5a1953e47f96e0e7646f017c14e2eb07e615d5
vucu <cxv@g.ucla.edu>
2017-01-17 23:50:41 -0800

    Correct subset implementation

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -1,3 +1,10 @@
+let rec inset item set = 
+  match set with 
+    | [] -> false
+    | h :: t -> if item = h 
+                  then true
+                  else inset item t;;
+
 let rec subset a b = 
   match a with
     | [] -> true
