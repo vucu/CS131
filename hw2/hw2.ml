675c98ee3049d72b7a8b5e69e3d91039952aafb4
vucu <cxv@g.ucla.edu>
2017-02-01 21:48:36 -0800

    Stable

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -19,10 +19,7 @@ let convert_grammar gram1 =
 	
 	
 (* patse_prefix *)
-(*
-	Concat Rule
-*)
-let rec make_concat_matcher rules rule accept derivation frag = 
+let rec make_a_matcher rules rule accept derivation frag = 
 	match rule with 
 	| [] -> accept derivation frag
 	| head :: tail -> 
@@ -32,25 +29,22 @@ let rec make_concat_matcher rules rule accept derivation frag =
 		(
 			match frag with 
 			| [] -> None
-			| frag_head :: frag_tail -> 
-				if (frag_head = terminal) then 
-					(make_concat_matcher rules tail accept derivation frag_tail)
+			| f_head :: f_tail -> 
+				if (f_head = terminal) then 
+					(make_a_matcher rules tail accept derivation f_tail)
 				else
 					None
 		)
 		| N(nonterminal) -> 
-			make_or_matcher rules (rules nonterminal) nonterminal (make_concat_matcher rules tail accept) frag derivation
+			make_or_matcher rules (rules nonterminal) nonterminal (make_a_matcher rules tail accept) frag derivation
 	)
 
-(*
-	OR Rule
-*)
 and make_or_matcher rules matching_rules lhs accept frag derivation = 
 	match matching_rules with
 	| [] -> None
 	| head :: tail -> 
 	(
-		match make_concat_matcher rules head accept (derivation @ [(lhs, head)]) frag with
+		match make_a_matcher rules head accept (derivation @ [(lhs, head)]) frag with
 		| None -> make_or_matcher rules tail lhs accept frag derivation
 		| any -> any 
 	)
