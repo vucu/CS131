e77232b9d200efb856123821c43f6cd1b1453977
vucu <cxv@g.ucla.edu>
2017-02-01 22:13:14 -0800

    Stable - Note that some of previous commit is unstable

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -15,11 +15,11 @@ let rec derive rules x =
 	  
 let convert_grammar gram1 = 
 	match gram1 with
-	| (s, rules) -> (s, (fun x -> (derive rules x)))
+	| (start, rules) -> (start, (fun x -> (derive rules x)))
 	
 	
-(* patse_prefix *)
-let rec make_a_matcher rules rule accept frag derivation = 
+(* parse_prefix *)
+let rec make_a_matcher rules rule accept derivation frag = 
 	match rule with 
 	| [] -> accept derivation frag
 	| head :: tail -> 
@@ -31,7 +31,7 @@ let rec make_a_matcher rules rule accept frag derivation =
 			| [] -> None
 			| f_head :: f_tail -> 
 				if (f_head = terminal) then 
-					(make_a_matcher rules tail accept frag_tail derivation)
+					(make_a_matcher rules tail accept derivation f_tail)
 				else
 					None
 		)
@@ -44,11 +44,11 @@ and make_or_matcher rules matching_rules lhs accept frag derivation =
 	| [] -> None
 	| head :: tail -> 
 	(
-		match make_a_matcher rules head accept frag (derivation @ [(lhs, head)]) with
+		match make_a_matcher rules head accept (derivation @ [(lhs, head)]) frag with
 		| None -> make_or_matcher rules tail lhs accept frag derivation
 		| any -> any 
 	)
 
 let parse_prefix grammar accept frag = 
   match grammar with
-    | (start_symbol, rules) -> make_or_matcher rules (rules start_symbol) start_symbol accept frag []
+    | (start, rules) -> make_or_matcher rules (rules start) start accept frag []
