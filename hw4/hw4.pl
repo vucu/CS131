122abac18ce2377c8b585fa2cf0c08c7a927ec44
Vu Cu <cxv@g.ucla.edu>
2017-02-24 00:26:31 -0800

    hw4 - break function

--- hw4/hw4.pl
+++ hw4/hw4.pl
@@ -3,4 +3,18 @@
 signal_morse([], []).
 
 % M is the match of the list if ????????
-signal_morse([H|T],M]:- true
+signal_morse([H|T],M):- 
+	break([H|T], B), 
+	valid(B, M).
+
+% break to list of 1s and 0s
+break([], []).
+break([First], [[First]]).
+break([First, Second | T], [[First] | Other]) :-
+	First \= Second, 
+	break([Second | T], Other),
+	!.
+break([First, First| T], [ [First|T2] | Other] ):-
+    break([First | T], [T2 | Other]),
+	!.
+	
