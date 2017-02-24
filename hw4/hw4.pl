3ebfa30de9cee33b34d28e75c20b81614d5faadf
Vu Cu <cxv@g.ucla.edu>
2017-02-24 00:40:17 -0800

    hw4 - Morse table

--- hw4/hw4.pl
+++ hw4/hw4.pl
@@ -32,3 +32,64 @@ run_morse([[0,0,0,0,0] | Other], ['#' | Remain]):- run_morse(Other, Remain).
 run_morse([[0,0,0,0,0 | _] | Other], ['#' | Remain]):- 
 	run_morse(Other, Remain).
 	
+
+% Implement signal_message
+morse(a, [.,-]).           % A
+morse(b, [-,.,.,.]).	   % B
+morse(c, [-,.,-,.]).	   % C
+morse(d, [-,.,.]).	   % D
+morse(e, [.]).		   % E
+morse('e''', [.,.,-,.,.]). % É (accented E)
+morse(f, [.,.,-,.]).	   % F
+morse(g, [-,-,.]).	   % G
+morse(h, [.,.,.,.]).	   % H
+morse(i, [.,.]).	   % I
+morse(j, [.,-,-,-]).	   % J
+morse(k, [-,.,-]).	   % K or invitation to transmit
+morse(l, [.,-,.,.]).	   % L
+morse(m, [-,-]).	   % M
+morse(n, [-,.]).	   % N
+morse(o, [-,-,-]).	   % O
+morse(p, [.,-,-,.]).	   % P
+morse(q, [-,-,.,-]).	   % Q
+morse(r, [.,-,.]).	   % R
+morse(s, [.,.,.]).	   % S
+morse(t, [-]).	 	   % T
+morse(u, [.,.,-]).	   % U
+morse(v, [.,.,.,-]).	   % V
+morse(w, [.,-,-]).	   % W
+morse(x, [-,.,.,-]).	   % X or multiplication sign
+morse(y, [-,.,-,-]).	   % Y
+morse(z, [-,-,.,.]).	   % Z
+morse(0, [-,-,-,-,-]).	   % 0
+morse(1, [.,-,-,-,-]).	   % 1
+morse(2, [.,.,-,-,-]).	   % 2
+morse(3, [.,.,.,-,-]).	   % 3
+morse(4, [.,.,.,.,-]).	   % 4
+morse(5, [.,.,.,.,.]).	   % 5
+morse(6, [-,.,.,.,.]).	   % 6
+morse(7, [-,-,.,.,.]).	   % 7
+morse(8, [-,-,-,.,.]).	   % 8
+morse(9, [-,-,-,-,.]).	   % 9
+morse(., [.,-,.,-,.,-]).   % . (period)
+morse(',', [-,-,.,.,-,-]). % , (comma)
+morse(:, [-,-,-,.,.,.]).   % : (colon or division sign)
+morse(?, [.,.,-,-,.,.]).   % ? (question mark)
+morse('''',[.,-,-,-,-,.]). % ' (apostrophe)
+morse(-, [-,.,.,.,.,-]).   % - (hyphen or dash or subtraction sign)
+morse(/, [-,.,.,-,.]).     % / (fraction bar or division sign)
+morse('(', [-,.,-,-,.]).   % ( (left-hand bracket or parenthesis)
+morse(')', [-,.,-,-,.,-]). % ) (right-hand bracket or parenthesis)
+morse('"', [.,-,.,.,-,.]). % " (inverted commas or quotation marks)
+morse(=, [-,.,.,.,-]).     % = (double hyphen)
+morse(+, [.,-,.,-,.]).     % + (cross or addition sign)
+morse(@, [.,-,-,.,-,.]).   % @ (commercial at)
+
+% Error.
+morse(error, [.,.,.,.,.,.,.,.]). % error - see below
+
+% Prosigns.
+morse(as, [.,-,.,.,.]).          % AS (wait A Second)
+morse(ct, [-,.,-,.,-]).          % CT (starting signal, Copy This)
+morse(sk, [.,.,.,-,.,-]).        % SK (end of work, Silent Key)
+morse(sn, [.,.,.,-,.]).          % SN (understood, Sho' 'Nuff)
