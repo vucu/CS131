5ec2eaefcf63b82b84fa2c28f9a6540507033d46
Vu Cu <cxv@g.ucla.edu>
2017-03-05 21:37:38 -0800

    bad-ld

--- hw5/hw5.scm
+++ hw5/hw5.scm
@@ -1,14 +1,22 @@
 #lang racket
 
-(define (null-ld? obj) 
-	(if (null? obj) #f 
-		(if (not (pair? obj)) #f
-			(if (not (eq? (car obj) (cdr obj))) #f
-				#t
-			)
+(define (bad-ld? obj) 
+	(if (null? obj) #t 
+		(if (not (pair? obj)) #t
+			#f
 		)
   	)
 )
+
+(define (null-ld? obj) 
+	(if (bad-ld? obj) #f
+		(if (not (eq? (car obj) (cdr obj))) #f
+				#t
+		)
+  	)
+)
+
+
 
 ; test
 (define ils (append '(a e i o u) 'y))
